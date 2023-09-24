c334ff8d44bee1d056cd6d13ebf5e87df7f73daa
Braulio <brauliovaz1220@gmail.com>
2023-09-24 11:12:50 -0600

    Actualizacion al modelo de BD

--- bdSanisima.sql
+++ bdSanisima.sql
@@ -11,12 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de datos: `sanisima`
 --
@@ -30,45 +24,41 @@ USE `sanisima`;
 --
 
 CREATE TABLE `usuarios` (
-  `userID` varchar(55) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `apellido` varchar(50) DEFAULT NULL,
-  `telefono` int(15) DEFAULT NULL,
+  `userID` varchar(300) PRIMARY KEY,
+  `password` varchar(300) DEFAULT NULL,
+  `nombre` varchar(300) DEFAULT NULL,
+  `apellido` varchar(300) DEFAULT NULL,
+  `telefono` varchar(300) DEFAULT NULL,
   `colonia` varchar(100) DEFAULT NULL,
-  `calle` varchar(100) DEFAULT NULL,
-  `num_ext` int(10) DEFAULT NULL,
+  `calle` varchar(300) DEFAULT NULL,
+  `num_ext` varchar(300) DEFAULT NULL,
   `CP` int(10) DEFAULT NULL,
-  `fecha_nac` date DEFAULT NULL
+  `fecha_nac` date DEFAULT NULL,
+  `fecha_reg` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+CREATE TABLE `metodos_pago` (
+  `cuenta` varchar(300) PRIMARY KEY,
+  `fecha_caducidad` date NOT NULL,
+  `codigo_seguridad` varchar(300) NOT NULL,
+  `usuario` varchar(300) NOT NULL,
+  CONSTRAINT `fk_usuario_metodo_pago` 
+    FOREIGN KEY (`usuario`) REFERENCES `usuarios`(`userID`) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE 
+);
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`userID`, `password`, `nombre`, `apellido`, `telefono`, `colonia`, `calle`, `num_ext`, `CP`, `fecha_nac`) VALUES
-('usuario1@example.com', 'contrasena1', 'Juan', 'Pérez', 1234567890, 'Colonia A', 'Calle 1', 123, 12345, '2000-01-15'),
-('usuario10@example.com', 'contrasena10', 'Sofía', 'Gómez', 2147483647, 'Colonia J', 'Calle 10', 777, 44444, '1993-04-18'),
-('usuario2@example.com', 'contrasena2', 'María', 'López', 2147483647, 'Colonia B', 'Calle 2', 456, 54321, '1995-08-25'),
-('usuario3@example.com', 'contrasena3', 'Carlos', 'González', 2147483647, 'Colonia C', 'Calle 3', 789, 67890, '1987-03-10'),
-('usuario4@example.com', 'contrasena4', 'Ana', 'Rodríguez', 1112223333, 'Colonia D', 'Calle 4', 101, 11111, '1999-11-05'),
-('usuario5@example.com', 'contrasena5', 'Luis', 'Martínez', 2147483647, 'Colonia E', 'Calle 5', 222, 54321, '1990-02-20'),
-('usuario6@example.com', 'contrasena6', 'Laura', 'Sánchez', 2147483647, 'Colonia F', 'Calle 6', 333, 77777, '1985-07-30'),
-('usuario7@example.com', 'contrasena7', 'Pedro', 'Díaz', 2147483647, 'Colonia G', 'Calle 7', 444, 88888, '2002-12-03'),
-('usuario8@example.com', 'contrasena8', 'Marta', 'Fernández', 2147483647, 'Colonia H', 'Calle 8', 555, 99999, '1997-06-15'),
-('usuario9@example.com', 'contrasena9', 'Ricardo', 'Lara', 2147483647, 'Colonia I', 'Calle 9', 666, 33333, '1980-09-12');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`userID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `usuarios` (`userID`, `password`, `nombre`, `apellido`, `telefono`, `colonia`, `calle`, `num_ext`, `CP`, `fecha_nac`, `fecha_reg`) VALUES
+('usuario1@example.com', 'contrasena1', 'Juan', 'Pérez', '1234567890', 'Colonia A', 'Calle 1', '123', 12345, '2000-01-15', CURDATE()),
+('usuario10@example.com', 'contrasena10', 'Sofía', 'Gómez', '2147483647', 'Colonia J', 'Calle 10', '777', 44444, '1993-04-18', CURDATE()),
+('usuario2@example.com', 'contrasena2', 'María', 'López', '2147483647', 'Colonia B', 'Calle 2', '456', 54321, '1995-08-25', CURDATE()),
+('usuario3@example.com', 'contrasena3', 'Carlos', 'González', '2147483647', 'Colonia C', 'Calle 3', '789', 67890, '1987-03-10', CURDATE()),
+('usuario4@example.com', 'contrasena4', 'Ana', 'Rodríguez', '1112223333', 'Colonia D', 'Calle 4', '101', 11111, '1999-11-05', CURDATE()),
+('usuario5@example.com', 'contrasena5', 'Luis', 'Martínez', '2147483647', 'Colonia E', 'Calle 5', '222', 54321, '1990-02-20', CURDATE()),
+('usuario6@example.com', 'contrasena6', 'Laura', 'Sánchez', '2147483647', 'Colonia F', 'Calle 6', '333', 77777, '1985-07-30', CURDATE()),
+('usuario7@example.com', 'contrasena7', 'Pedro', 'Díaz', '2147483647', 'Colonia G', 'Calle 7', '444', 88888, '2002-12-03', CURDATE()),
+('usuario8@example.com', 'contrasena8', 'Marta', 'Fernández', '2147483647', 'Colonia H', 'Calle 8', '555', 99999, '1997-06-15', CURDATE()),
+('usuario9@example.com', 'contrasena9', 'Ricardo', 'Lara', '2147483647', 'Colonia I', 'Calle 9', '666', 33333, '1980-09-12', CURDATE());
